4448a8bad409c281bf30054c5ac3619a0041219c
dragonflyinmypool <jheiney10@gmail.com>
2022-12-28 12:25:27 -0500

    hook

---
+++ hook.js
@@ -0,0 +1,27 @@
+// Require the Express web framework and body-parser
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Create a new Express application
+const app = express();
+
+// Configure our Express application to parse incoming JSON
+app.use(bodyParser.json());
+
+// Create a route for our webhook
+app.post("/webhook", (request, response) => {
+  // Retrieve the request body
+  const { body } = request;
+
+  // Execute the webhook logic
+  // ...
+
+  // Return a response
+  response.json({
+    success: true,
+    message: "Webhook executed successfully!",
+  });
+});
+
+// Start the Express application
+app.listen(3000);
