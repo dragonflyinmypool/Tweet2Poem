a13eb8a612e029626ad6957040c1e027c0a6e053
dragonflyinmypool <jheiney10@gmail.com>
2022-12-30 11:30:33 -0500

    a

--- hook.js
+++ hook.js
@@ -17,7 +17,48 @@ app.get("/webhook", (req, res) => {
   const response = {
     response_token: `sha256=${response_token}`,
   };
+  const axios = require("axios");
 
+  let url = "https://api.twitter.com/1.1/account_activity/webhooks.json";
+  let params = {
+    url: "https://twithook.up.railway.app/webhook",
+  };
+  let authorization = {
+    oauth_consumer_key: "mHVi5kfGfWtIwap7khW8Stmq1",
+    oauth_nonce: "GENERATED",
+    oauth_signature_method: "HMAC-SHA1",
+    oauth_timestamp: "GENERATED",
+    oauth_token: "ACCESS_TOKEN",
+    oauth_version: "1.0",
+  };
+
+  axios
+    .request({
+      method: "post",
+      url:
+        url +
+        "?oauth_consumer_key=" +
+        authorization.oauth_consumer_key +
+        "&oauth_nonce=" +
+        authorization.oauth_nonce +
+        "&oauth_signature_method=" +
+        authorization.oauth_signature_method +
+        "&oauth_timestamp=" +
+        authorization.oauth_timestamp +
+        "&oauth_token=" +
+        authorization.oauth_token +
+        "&oauth_version=" +
+        authorization.oauth_version,
+      data: params,
+    })
+    .then((response) => {
+      console.log(response.statusCode);
+      console.log(response.headers);
+      console.log(response.body);
+    })
+    .catch((error) => {
+      throw error;
+    });
   console.log("server pinged");
   // Respond with 200 status code and the response token
   res.status(200).json(response);
@@ -40,7 +81,7 @@ app.put("/webhook", (req, res) => {
 });
 
 // Start the server on port 3000
-const port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0");
+// const port = process.env.PORT || 3000;
+// app.listen(port, "0.0.0.0");
 
-// app.listen(3000);
+app.listen(3000);
