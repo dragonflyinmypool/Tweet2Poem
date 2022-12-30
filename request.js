58aa4650ffb5f8420268d737d0e4fa47f4c00b2c
dragonflyinmypool <jheiney10@gmail.com>
2022-12-30 11:22:16 -0500

    changed request

---
+++ request.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+
+let url = "https://api.twitter.com/1.1/account_activity/webhooks.json";
+let params = {
+  url: "https://twithook.up.railway.app/webhook",
+};
+let authorization = {
+  oauth_consumer_key: "mHVi5kfGfWtIwap7khW8Stmq1",
+  oauth_nonce: "GENERATED",
+  oauth_signature_method: "HMAC-SHA1",
+  oauth_timestamp: "GENERATED",
+  oauth_token: "ACCESS_TOKEN",
+  oauth_version: "1.0",
+};
+
+axios
+  .request({
+    method: "post",
+    url:
+      url +
+      "?oauth_consumer_key=" +
+      authorization.oauth_consumer_key +
+      "&oauth_nonce=" +
+      authorization.oauth_nonce +
+      "&oauth_signature_method=" +
+      authorization.oauth_signature_method +
+      "&oauth_timestamp=" +
+      authorization.oauth_timestamp +
+      "&oauth_token=" +
+      authorization.oauth_token +
+      "&oauth_version=" +
+      authorization.oauth_version,
+    data: params,
+  })
+  .then((response) => {
+    console.log(response.statusCode);
+    console.log(response.headers);
+    console.log(response.body);
+  })
+  .catch((error) => {
+    throw error;
+  });
