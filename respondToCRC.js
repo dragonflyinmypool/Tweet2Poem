32be6966b1ef6e02138a4cb4a41902e78b9c78d2
dragonflyinmypool <jheiney10@gmail.com>
2023-01-06 14:08:44 -0500

    set up webserver

--- respondToCRC.js
+++ respondToCRC.js
@@ -6,7 +6,34 @@ const app = express();
 
 app.get('/webhook', (req, res) => {
   console.log('Server received a request on /webhook');
-  res.send('You are visiting /webhook ' + process.env.MESSAGE);
+  res.send(`
+  <section>
+    <h3>Welcome to the API, the consumer key is: </h3>
+    <pre>  
+      <code>${process.env.TWITTER_CONSUMER_SECRET}</code>
+    </pre>
+    </section>
+    <style>
+    * {
+      margin: 0;
+      padding: 0;
+    }
+    html {
+      background-color:black;
+      color: white;
+    }
+    section {
+      padding: 25px;
+    }
+    pre code {
+      background-color: #eee;
+      border: 1px solid #999;
+      display: block;
+      padding: 15px;
+      color: black;
+    }
+    </style>
+  `);
 });
 
 app.listen(3000);
