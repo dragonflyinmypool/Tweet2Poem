5b1674a8be0ed199b4757de896b556b23fa4c920
dragonflyinmypool <jheiney10@gmail.com>
2023-01-06 14:25:13 -0500

    API setup to repond to crc check

--- respondToCRC.js
+++ respondToCRC.js
@@ -1,40 +1,42 @@
 const express = require('express');
 require('dotenv').config();
 
+// Get my consumer secret from the env file
+const consumer_secret = process.env.TWITTER_CONSUMER_SECRET;
+
 // Start express
 const app = express();
 
 app.get('/webhook', (req, res) => {
   console.log('Server received a request on /webhook');
-  res.send(`
-  <section>
-    <h3>Welcome to the API, the consumer key is: </h3>
-    <pre>  
-      <code>${process.env.TWITTER_CONSUMER_SECRET}</code>
-    </pre>
-    </section>
-    <style>
-    * {
-      margin: 0;
-      padding: 0;
-    }
-    html {
-      background-color:black;
-      color: white;
-    }
-    section {
-      padding: 25px;
-    }
-    pre code {
-      background-color: #eee;
-      border: 1px solid #999;
-      display: block;
-      padding: 15px;
-      color: black;
-    }
-    </style>
-  `);
+
+  // Get the crc_token from twitter request
+  const crc_token = req.query.crc_token;
+
+  // Calculate the response token, based on the crc_token and consumer secret
+  const response_token = calculate_response(crc_token, consumer_secret);
+
+  // Format response
+  const reponse = {
+    response_token: `sha256=${response_token}`,
+  };
+
+  // Respond with 200 status code and the response token
+  res.status(200).json(response);
 });
+
+calculate_response = function (crc_token, consumer_secret) {
+  hmac = crypto
+    .createHmac('sha256', consumer_secret)
+    .update(crc_token)
+    .digest('base64');
+
+  console.log('crc_token: ' + crc_token);
+  console.log('consumer_secret: ' + consumer_secret);
+  console.log('hmac: ' + hmac);
+
+  return hmac;
+};
 
 app.listen(3000);
 console.log('Server is listening for requests');
